7fe6ef4cc8cbffa0df3140f58a37184adbf3eb8f
YUDAI YAMASHITA <70077254+yudaiyamashita@users.noreply.github.com>
2021-03-20 06:17:11 +0900

    Add files via upload

---
+++ Main.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import ContactForm from './ContactForm';
+
+class Main extends React.Component {
+
+
+    return (
+
+        
+
+          <div className='contact-container'>
+            <h3>お問い合わせ</h3>
+            <ContactForm />
+          </div>
+
+
+    );
+  }
+
+
+export default Main;
